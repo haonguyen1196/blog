1a8f836e75ef865ed64dc0b590908a5a7f5c4883
Your Name <you@example.com>
2023-04-02 15:21:30 +0700

    intall prettier, lint-staged, husky

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+// Template Enginer
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+route(app);
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
